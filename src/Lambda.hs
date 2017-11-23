7b2887fc6ba4676661b34abcc2bc8a386173d88f
Unknown <pandaplox@gmail.com>
2017-11-23 16:29:19 -0500

    Change Up

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,1 +1,12 @@
-import 
+module LambdaLexer where
+    import Text.Parsec
+    import Text.Parsec.Combinator (between, sepBy1, chainr1)
+    import Data.List (elemIndex)
+
+
+    -- Simply Typed λ-Calculus Evaluator
+    -- http://www.ling.ohio-state.edu/~pollard.4/type/books/pierce-tpl.pdf, page 113
+    -- Grammar for λ-terms in our implementation:
+    --      M, N ::= x | λx . M | M N
+
+    
